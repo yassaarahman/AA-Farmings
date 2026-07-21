a101d9246ad35b199ebcdb994173e8dde29c854c
Muhammad Yassaa Rahman <yassaarahman@gmail.com>
2026-07-21 12:05:43 +0500

    Added routes for "Add-Product" & "Edit-Product"

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,20 +4,20 @@ const {
   getAddProduct,
   postAddProduct,
   getAdminProductList,
+  getEditProduct,
 } = require("../controllers/adminController");
-
-adminRouter.get("/add-Product", getAddProduct);
-
-adminRouter.post("/add-Product", postAddProduct);
 
 adminRouter.get("/admin-home", (req, res, next) => {
   res.render("admin/admin-home", { pageTitle: "Home | Admin" });
 });
 
-adminRouter.get("/edit-product", (req, res) => {
-  res.render("admin/edit-product", { pageTitle: "Edit Product | Admin" });
-});
+adminRouter.get("/edit-Product", getAddProduct);
 
-adminRouter.get("/admin-product-list", getAdminProductList)
+adminRouter.get("/edit-Product/:productID", getEditProduct);
+
+adminRouter.get("/add-Product", getAddProduct);
+adminRouter.post("/add-Product", postAddProduct);
+
+adminRouter.get("/admin-product-list", getAdminProductList);
 
 exports.adminRouter = adminRouter;
