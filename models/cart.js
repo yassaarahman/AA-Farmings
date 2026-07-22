2f84782e9034957553fc3c016ab22b424c0aa939
Muhammad Yassaa Rahman <yassaarahman@gmail.com>
2026-07-22 22:09:19 +0500

    Refactor cart file path variable for consistency

--- models/cart.js
+++ models/cart.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const rootDir = require("../utils/pathUtil");
 
-const cartPath = path.join(rootDir, "data", "cart.json");
+const cartDataPath = path.join(rootDir, "data", "cart.json");
 
 module.exports = class Cart {
   static addToCart(productID, callback) {
@@ -11,13 +11,13 @@ module.exports = class Cart {
         callback("Product is already in the Cart");
       } else {
         cartProducts.push(productID);
-        fs.writeFile(cartPath, JSON.stringify(cartProducts), callback);
+        fs.writeFile(cartDataPath, JSON.stringify(cartProducts), callback);
       }
     });
   }
 
   static getCart(callback) {
-    fs.readFile(cartPath, (err, data) => {
+    fs.readFile(cartDataPath, (err, data) => {
       callback(!err ? JSON.parse(data) : []);
     });
   }
